f595a11a5d2db26a4734f4ef92992763abf3de27
YoungJae <y2336789@naver.com>
2021-05-22 13:30:31 +0900

    session과 locals, cookie를 이용한 로그인

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,7 +13,17 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+
+app.use(
+    session({
+      secret: "Hello!",
+      resave: true,
+      saveUninitialized: true,
+    })
+);
+  
+app.use(localsMiddleware);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
