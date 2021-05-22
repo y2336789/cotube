b8d470930d8d30d34e126f85ed09d18234180797
YoungJae <y2336789@naver.com>
2021-05-22 23:31:27 +0900

    로그인, 로그아웃 프로필 구현

--- src/server.js
+++ src/server.js
@@ -16,14 +16,14 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    session({
-      secret: "Hello!",
-      resave: true,
-      saveUninitialized: true,
-      store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/cotube" }),
-    })
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
 );
-  
+
 app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
